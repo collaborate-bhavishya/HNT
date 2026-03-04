f56fbb1bb8c3bfda2ed886e0a6660445f94a77ed
collaborate-bhavishya <collaborate.bhavishya@gmail.com>
2026-03-04 10:33:42 +0530

    fix(tests): Mock backend services for default NestJS unit test files

--- backend/src/admin/admin.service.spec.ts
+++ backend/src/admin/admin.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { PrismaService } from '../prisma.service';
 
 describe('AdminService', () => {
   let service: AdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdminService],
+      providers: [
+        AdminService,
+        {
+          provide: PrismaService,
+          useValue: {
+            candidate: {
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AdminService>(AdminService);
